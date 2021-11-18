96e857130c852ea16e819020b43f4542b2bb47da
Evan Duncan <evan.e.duncan@gmail.com>
2021-11-18 11:41:03 -0600

    Add monolog example

--- test.php
+++ test.php
@@ -4,8 +4,19 @@ require_once './vendor/autoload.php';
 
 use Psr\Http\Message\{RequestInterface, ResponseInterface};
 use React\Http\Message\Response;
+use Monolog\Logger;
+use Monolog\Handler\ErrorLogHandler;
+use GuzzleHttp\Psr7\Message;
+
+$logger = new Logger('app');
+$logger->pushHandler(new ErrorLogHandler());
 
 $app = new \Lucia\App();
+
+$app->use(function (RequestInterface $request, callable $next) use($logger) {
+    $logger->info(Message::toString($request));
+    return $next($request);
+});
 
 $app->use(function (RequestInterface $request, callable $next) {
     $request = $request->withHeader('X-Request-ID', bin2hex(random_bytes(32)));
@@ -21,5 +32,4 @@ $app->get('/', function (RequestInterface $request): ResponseInterface {
 });
 
 $app->listen('127.0.0.1:8080');
-
-echo "Server running at http://127.0.0.1:8080" . PHP_EOL;
+$logger->info("Server running at http://localhost:8080");
